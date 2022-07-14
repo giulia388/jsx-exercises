713b22e97cc9cf92afd12f2aae87f3a186fdb011
Giulia Colucci <giuliacolucci388@gmail.com>
2022-07-14 17:29:12 +0200

    add Hello World!

--- jsx_exercises/src/HelloWorld.js
+++ jsx_exercises/src/HelloWorld.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default class HelloWorld extends React.Component{
-    hello = 'Hello World';
+    hello = 'Hello, World!';
     render(){
         return<h1>{this.hello}</h1>
     }
