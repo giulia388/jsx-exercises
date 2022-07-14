792102637705f6859aaaf6d4f62647bfaea60d28
Giulia Colucci <giuliacolucci388@gmail.com>
2022-07-14 17:14:22 +0200

    add hello world

---
+++ jsx_exercises/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import { HelloWorld } from './HelloWorld';
+
+class App extends React.Component{
+
+  render() {
+
+    return (
+      <div>
+        <HelloWorld/>
+      </div>
+    )
+  }
+}
+
+export default App;
